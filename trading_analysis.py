5e26b8a8b7fecd16a2519e44612e2ab42dbec18a
qingluo <luoqing222@gmail.com>
2015-03-07 12:48:30 -0800

    second

--- trading_analysis.py
+++ trading_analysis.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
 
     gm = emailprocessing.Gmail('raki1978wmc6731@gmail.com', 'Fapkc1897Fapkc')
 
+
     gm.send_message('Subject', 'Message', 'luoqing222@gmail.com')
     # main()
 
