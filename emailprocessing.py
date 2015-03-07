4455520ed36efbbf666d1fee3fde40aa9eea50e2
qingluo <luoqing222@gmail.com>
2015-03-07 12:43:40 -0800

    first version

---
+++ emailprocessing.py
@@ -0,0 +1,31 @@
+__author__ = 'qingluo'
+
+import smtplib
+
+
+class Gmail(object):
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.server = 'smtp.gmail.com'
+        self.port = 587
+        session = smtplib.SMTP(self.server, self.port)
+        session.ehlo()
+        session.starttls()
+        session.ehlo
+        session.login(self.email, self.password)
+        self.session = session
+
+    def send_message(self, subject, body, to):
+        ''' This must be removed '''
+        headers = [
+            "From: " + self.email,
+            "Subject: " + subject,
+            "To: " + to,
+            "MIME-Version: 1.0",
+            "Content-Type: text/html"]
+        headers = "\r\n".join(headers)
+        self.session.sendmail(
+            self.email,
+            to,
+            headers + "\r\n\r\n" + body)
---
+++ trading_analysis.py
@@ -0,0 +1,12 @@
+__author__ = 'qingluo'
+
+import emailprocessing
+
+
+if __name__ == "__main__":
+
+    gm = emailprocessing.Gmail('raki1978wmc6731@gmail.com', 'Fapkc1897Fapkc')
+
+    gm.send_message('Subject', 'Message', 'luoqing222@gmail.com')
+    # main()
+
