4e16941578a678a4d45a2dc5928773cabfb88c0f
qingluo <luoqing222@gmail.com>
2015-03-24 22:05:16 -0700

    second

--- trading_analysis.py
+++ trading_analysis.py
@@ -189,6 +189,8 @@ def initialize_index_fund_table():
 
 def initialize_sp500_table():
     models.db.connect()
+    file_name = os.path.dirname(sys.argv[0]) + "/" + "constituents.csv"
+        print file_name
     if not models.Sp500Symbol.table_exists():
         models.db.create_table(models.Sp500Symbol)
         file_name = os.path.dirname(sys.argv[0]) + "/" + "constituents.csv"
