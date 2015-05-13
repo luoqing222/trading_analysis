7e001c9f5f4333d4d4db29067fd10bbfdb9dc33c
qingluo <luoqing222@gmail.com>
2015-05-12 21:31:10 -0700

    second

--- backtesting_helper.py
+++ backtesting_helper.py
@@ -22,34 +22,52 @@ import trading_date_utility
 
 if __name__ == "__main__":
 
-    #first get the holiday in 2014
-    #the rule to tell if a holiday is holiday is that no spy data on that day but is not weekend
-
-
-
-    #start_date=
-
-
-
-    #data_analyser = yahoo_data_analyser.YahooEquityDataAnalyser()
-    #file_name = "sp500_daily_rsq_"+datetime.datetime.now().strftime('%m_%d_%Y')+".csv"
-    #data_analyser.calculate_daily_rsq(symbol_list,index_list, None, 30, file_name)
-    #send_email(file_name)
-
-    # function to generate "constituents_wiki.csv"
     data_manager= trading_data_management.TradingDataManager()
-    # start_date="2015/04/08"
-    # end_date="2015/05/08"
-    # dates=trading_date_utility.data_available_dates(start_date, end_date, "spy")
-    # for date in dates:
-    #     (year, week, weekday) = date.isocalendar()
-    #     print str(weekday)
-
     data_manager.populate_historical_holiday()
-    # data_manager.generate_constituents("constituents_wiki.csv")
-    #data_manager.delete_holiday(2015,4,3,"US")
-    #data_manager.add_holiday(2015,4,3,"US")
-    #data_manager.populate_holiday()
+
+
+
+
+    # find the tick that in the HistoricalPrice
+    query = models.HistoricalPrice.select(models.HistoricalPrice.symbol,
+                                          fn.Max(models.HistoricalPrice.transaction_date).
+                                          alias('recent_date')).group_by(models.HistoricalPrice.symbol)
+
+    symbol_most_recent_date = {}
+    # mapping from symbol to most recent date is saved in dictionary symbol_most_recent_date
+    # including the index fund price
+    for item in query:
+        symbol_most_recent_date[item.symbol] = item.recent_date
+
+    # get the SP500 list with most recent save date
+    query = models.Sp500Symbol.select(fn.Max(models.Sp500Symbol.save_date).alias('recent_date'))
+    for item in query:
+        SP500_recent_date = item.recent_date
+
+    # item in the query is latest sp500 tick
+    symbol_list= []
+    query = models.Sp500Symbol.select().where(models.Sp500Symbol.save_date == SP500_recent_date)
+    for item in query:
+        symbol = item.symbol
+        #save_trading_data(symbol, symbol_most_recent_date)
+        symbol_list.append(symbol)
+
+    #do the same thing for index fund
+    query = models.IndexSymbol.select()
+    index_list= []
+    for item in query:
+        symbol = item.symbol
+        #save_trading_data(symbol, symbol_most_recent_date)
+        index_list.append(symbol)
+
+    start_date="2015/04/08"
+    end_date="2015/05/08"
+    data_analyser = yahoo_data_analyser.YahooEquityDataAnalyser()
+    file_name = "sp500_historical_rsq.csv"
+    data_analyser.calculate_historical_rsq(symbol_list,index_list,start_date,end_date, 30, file_name)
+
+
+
 
 
 
