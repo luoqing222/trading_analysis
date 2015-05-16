72b7c5f4ed7746e288f5ff99ab38bb9571a40bcb
qingluo <luoqing222@gmail.com>
2015-05-15 22:26:27 -0700

    second

---
+++ wiki_data_loader.py
@@ -0,0 +1,46 @@
+import urllib
+import BeautifulSoup
+
+__author__ = 'qingluo'
+
+
+class WikiDataLoader:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_latest_sp500():
+        try:
+            link = "http://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+            html_text = urllib.urlopen(link)
+            soup = BeautifulSoup.BeautifulSoup(html_text)
+            table = soup.find('table', {'class': 'wikitable sortable'})
+            th = table.findAll('th')
+            td = table.findAll('td')
+            result = []
+            for i in range(0, len(td) / len(th)):
+                index = len(th) * i + 0
+                result.append(td[index].text)
+            return result
+        except:
+            return None
+
+
+    @staticmethod
+    def generate_constituents(file_name):
+        file = open(file_name, "w")
+        link = "http://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+        html_text = urllib.urlopen(link)
+        soup = BeautifulSoup.BeautifulSoup(html_text)
+        table = soup.find('table', {'class': 'wikitable sortable'})
+        th = table.findAll('th')
+        for head in th:
+            print head.text
+        print len(th)
+        td = table.findAll('td')
+        for i in range(0, len(td) / len(th)):
+            for j in range(0, len(th)):
+                index = len(th) * i + j
+                file.write(td[index].text + ",")
+            file.write("\n")
+        file.close()
