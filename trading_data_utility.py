81646bab63e45e077b85dbdde77220a48b3686df
qingluo <luoqing222@gmail.com>
2015-05-17 17:57:29 -0700

    second

--- trading_data_utility.py
+++ trading_data_utility.py
@@ -10,7 +10,7 @@ from datetime import timedelta
 import peewee
 
 
-#this class is used for the simple data utility functionality
+# this class is used for the simple data utility functionality
 #such as delete, update data tables
 class TradingDataUtility:
     def __init__(self):
@@ -18,12 +18,12 @@ class TradingDataUtility:
 
 
     @staticmethod
-    def delete_holiday(year, month, day,country):
+    def delete_holiday(year, month, day, country):
         models.db.connect()
 
         holiday = datetime.datetime(year, month, day)
-        query= models.HolidayCalendar.delete().where(models.HolidayCalendar.date== holiday,
-                                                     models.HolidayCalendar.country_code==country)
+        query = models.HolidayCalendar.delete().where(models.HolidayCalendar.date == holiday,
+                                                      models.HolidayCalendar.country_code == country)
         query.execute()
 
 
@@ -34,10 +34,10 @@ class TradingDataUtility:
             models.db.create_table(models.HolidayCalendar)
 
         holiday = datetime.datetime(year, month, day)
-        models.HolidayCalendar.create(date=holiday, country_code= country)
+        models.HolidayCalendar.create(date=holiday, country_code=country)
 
 
-#function to populate the holiday into database table
+    #function to populate the holiday into database table
     @staticmethod
     def populate_holiday():
         models.HolidayCalendar.drop_table()
@@ -50,60 +50,69 @@ class TradingDataUtility:
         TradingDataManager.add_holiday(2015, 11, 27, "US")
         TradingDataManager.add_holiday(2015, 12, 25, "US")
 
-#function to populate the historical holiday into database table
-#rule: is the dates has SPY data, we consider it as trading date, spy data staring from 1993/01/29
+    #function to populate the historical holiday into database table
+    #rule: is the dates has SPY data, we consider it as trading date, spy data staring from 1993/01/29
     @staticmethod
     def populate_historical_holiday():
-        trading_dates=sets.Set([])
-        start_date="1983/01/01"
-        update_date="2015/05/09"
-        symbol="spy"
+        trading_dates = sets.Set([])
+        start_date = "1983/01/01"
+        update_date = "2015/05/09"
+        symbol = "spy"
         data_record = models.HistoricalPrice.select().where((models.HistoricalPrice.symbol == symbol)
-                                        & (models.HistoricalPrice.transaction_date >= start_date)
-        & (models.HistoricalPrice.transaction_date <= update_date)).order_by(models.HistoricalPrice.transaction_date)
+                                                            & (models.HistoricalPrice.transaction_date >= start_date)
+                                                            & (
+                                                                models.HistoricalPrice.transaction_date <= update_date)).order_by(
+            models.HistoricalPrice.transaction_date)
         #print data_record.count()
-        for i in range(0,data_record.count()):
+        for i in range(0, data_record.count()):
             trading_dates.add(data_record[i].transaction_date.toordinal())
-        for date in range(min(trading_dates),max(trading_dates)):
+        for date in range(min(trading_dates), max(trading_dates)):
             if date not in trading_dates:
-                nonordinal_date= datetime.date.fromordinal(date)
+                nonordinal_date = datetime.date.fromordinal(date)
                 (year, week, weekday) = nonordinal_date.isocalendar()
                 if weekday < 6:
                     print nonordinal_date.year, nonordinal_date.month, nonordinal_date.day
                     try:
-                        TradingDataManager.add_holiday(nonordinal_date.year, nonordinal_date.month, nonordinal_date.day, "US")
+                        TradingDataManager.add_holiday(nonordinal_date.year, nonordinal_date.month, nonordinal_date.day,
+                                                       "US")
                     except peewee.IntegrityError:
                         pass
 
-#function to run query on database table Sp500List
+                    #function to run query on database table Sp500List
+
     @staticmethod
     def create_sp500_list():
         models.db.create_table(models.Sp500List)
 
     @staticmethod
-    def add_symbol(date,symbol):
-        models.Sp500List.create(save_date= date,symbol= symbol)
+    def add_symbol(date, symbol):
+        models.Sp500List.create(save_date=date, symbol=symbol)
 
     @staticmethod
     def get_sp500_list(date):
-        sp_list=[]
+        sp_list = []
         for sp in models.Sp500List.select(models.Sp500List.save_date).distinct():
             sp_list.append(sp.save_date)
         sp_list.sort()
-        save_date=sp_list[-1]
-        if sp_list[0]>date:
-            save_date=sp_list[0]
+        save_date = sp_list[-1]
+        if sp_list[0] > date:
+            save_date = sp_list[0]
         else:
-            for i in range(0,len(sp_list)-1):
-                if sp_list[i]<=date and sp_list[i+1]>date:
-                    save_date= sp_list[i]
+            for i in range(0, len(sp_list) - 1):
+                if sp_list[i] <= date and sp_list[i + 1] > date:
+                    save_date = sp_list[i]
 
-        sp500_list=models.Sp500List.select().where(models.Sp500List.save_date == save_date)
-        result=[]
+        sp500_list = models.Sp500List.select().where(models.Sp500List.save_date == save_date)
+        result = []
         for it in sp500_list:
             result.append(it.symbol)
         return result
 
+
+#functions to manage the strong stock table
+    @staticmethod
+    def create_strong_stock_table():
+        models.db.create_table(models.StrongStock)
 
 
 
