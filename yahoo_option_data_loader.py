008f3724298477dcf970dd4994b11caf362ac9e5
qingluo <luoqing222@gmail.com>
2015-05-31 21:45:43 -0700

    second

--- yahoo_option_data_loader.py
+++ yahoo_option_data_loader.py
@@ -2,9 +2,7 @@ __author__ = 'qingluo'
 
 import BeautifulSoup
 import urllib
-import re
-import time
-
+import datetime
 
 class YahooOptionDataLoader:
     def __init__(self):
@@ -16,33 +14,40 @@ class YahooOptionDataLoader:
         link = "http://finance.yahoo.com/q/op?s=" + symbol + "+Options"
         return link
 
+    # function to get the option data for one expire date
+    # the header in the file is under, type, expire date, strike, contract name, last, bid, ask, change, %change,
+    # volume, open interest, implied volatility
     @staticmethod
-    def find_all_options_for_one_expire_date(web_page):
-        html_text = urllib.urlopen(web_page)
-        soup = BeautifulSoup.BeautifulSoup(html_text)
-        for option_type in soup.findAll('caption'):
-            option_table= option_type.parent
-            for row in option_table.findAll("tr"):
-                if row.has_key("data-row"):
-                    cols = row.findAll("td")
-                    cols = [ele.text.strip() for ele in cols]
-                    print cols
+    def find_all_options_for_one_expire_date(web_page, expire_date, symbol,output_file):
+        try:
+            html_text = urllib.urlopen(web_page)
+            soup = BeautifulSoup.BeautifulSoup(html_text)
+            for option_type in soup.findAll('caption'):
+                option_table = option_type.parent
+                for row in option_table.findAll("tr"):
+                    if row.has_key("data-row"):
+                        output_file.write(symbol+",")
+                        output_file.write(option_type.string.strip() + ",")
+                        output_file.write(expire_date + ",")
+                        cols = row.findAll("td")
+                        for col in cols:
+                            output_file.write(col.text.strip() + ",")
+                        output_file.write("\n")
+        except:
+            pass
 
-
-
-            #print "Option Type " + option_type.string.strip()
-
-
-    def web_crawler(self, symbol):
+    # function to find the option data for the symbol list
+    def web_crawler(self, symbol, file):
+        print "loading yahoo option data for "+symbol
         html_text = urllib.urlopen(self.get_web_page_link(symbol))
         soup = BeautifulSoup.BeautifulSoup(html_text)
         for expire_date_link in soup.findAll("option"):
             if len(expire_date_link['data-selectbox-link']) != 0:
                 expire_date = expire_date_link.string.strip()
                 link = "http://finance.yahoo.com/" + expire_date_link['data-selectbox-link']
-                self.find_all_options_for_one_expire_date(link)
-
-                #print time.strptime(expire_date_link.string.strip(), "%B %d, %Y")
+                expire_date = datetime.datetime.strptime(expire_date, "%B %d, %Y")
+                expire_date = expire_date.strftime("%Y/%m/%d")
+                self.find_all_options_for_one_expire_date(link, expire_date, symbol,file)
 
 
 
--- yahoo_option_data_loader_test.py
+++ yahoo_option_data_loader_test.py
@@ -6,13 +6,12 @@ import time
 if __name__ == "__main__":
 
     start_time = time.time()
+    file=open("test.csv","w")
     data_loader=yahoo_option_data_loader.YahooOptionDataLoader()
-
-    #symbols=["FB","MMM","MCO","BF-B","O"]
-    symbols=["FB"]
+    symbols=["FB","MMM","MCO","BF-B","O"]
+    #symbols=["FB"]
     for symbol in symbols:
-        data_loader.web_crawler(symbol)
-
+        data_loader.web_crawler(symbol,file)
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
