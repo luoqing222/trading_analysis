1f6e50d6bbad93434c5ccc5ac0cee49b914559eb
Qing <luoqing222@gmail.com>
2015-08-30 22:43:42 -0700

    add the functionality to upload the 1min bar data into database

--- data_uploader/eod_1minbar_data_uploader.py
+++ data_uploader/eod_1minbar_data_uploader.py
@@ -1,1 +1,76 @@
 __author__ = 'Qing'
+
+import os
+import models
+import datetime
+import MySQLdb
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Eod1MinBarDataUploader:
+    def __init__(self, host,database, user, password, folder):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.folder = folder
+        self.file_lists = ['NASDAQ', 'NYSE']
+
+    def upload_bar_1min_equity_to_db(self, host, database, user, password, file_name, folder):
+        ''' function to save 1 minute frequency trading data into database
+        :param config: config object
+        :param file_name: file name need to start with NASDAQ or NYSE
+        :param folder:
+        :return: None
+        '''
+        models.db.connect()
+        if not models.Bar1MinEodData.table_exists():
+            models.db.create_table(models.Bar1MinEodData)
+        des_file_name = folder + "/" + file_name
+        exchange = file_name.split("_")[0]
+        records = []
+        if os.path.exists(des_file_name):
+            with open(des_file_name) as fp:
+                next(fp)
+                for line in fp:
+                    splited_item = line.split(',')
+                    if len(splited_item) == 7:
+                        [symbol, transaction_time, open_price, high_price, low_price, close_price,
+                         volume] = splited_item
+                        transaction_time = datetime.datetime.strptime(transaction_time,'%d-%b-%Y %H:%M')
+                        transaction_date = transaction_time.strftime("%Y-%m-%d")
+                        transaction_time = transaction_time.strftime("%Y-%m-%d %H:%M")
+                        try:
+                            records.append((symbol, transaction_date, transaction_time,float(open_price), float(high_price),
+                                            float(low_price), float(close_price), int(volume.strip()), exchange))
+                        except:
+                            pass
+
+        #host = config.get("database", "host")
+        #database = config.get("database", "database")
+        #user = config.get("database", "user")
+        #password = config.get("database", "passwd")
+        db = MySQLdb.connect(host=host, db=database, user=user, passwd=password)
+        cursor = db.cursor()
+        sql_statement = "insert into bar1mineoddata(symbol,transaction_date,transaction_time, open_price, high_price,low_price, close_price,volume, exchange) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        try:
+            cursor.executemany(sql_statement, records)
+            db.commit()
+        except:
+            db.rollback()
+            raise
+        finally:
+            cursor.close()
+            db.close()
+
+    def run(self, running_time):
+        des_folder = self.folder+ "/daily_run/" + running_time.strftime('%Y_%m_%d')+"/eod"
+        for file_name in self.file_lists:
+            des_file_name= file_name + "_BAR_1MIN_"+ running_time.strftime('%Y%m%d') +".csv"
+            self.upload_bar_1min_equity_to_db(self.host, self.database, self.user, self.password, des_file_name, des_folder)
+            logger.info("%s is successfully uploaded", des_folder+"/"+des_file_name)
+
+    def historical_run(self):
+        pass
+
---
+++ tests/eod_1minbar_data_uploader_test.py
@@ -0,0 +1,28 @@
+__author__ = 'Qing'
+
+import logging
+import configparser
+import datetime
+
+from data_uploader import eod_1minbar_data_uploader
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+
+    config_file = "../option_data_management_setting.ini"
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    host = config.get("database", "host")
+    database = config.get("database", "database")
+    user = config.get("database", "user")
+    password = config.get("database", "passwd")
+    des_folder = config.get("csv","data_folder")
+    data_uploader = eod_1minbar_data_uploader(host, database, user,password, des_folder)
+    logging.basicConfig(filename='test.log', level=logging.INFO,filemode="w")
+    logger.info("begin eod 1min bar data upload test")
+
+    #running_time = datetime.datetime.now()
+    running_time = datetime.datetime(year=2015, month=8, day=28)
+    data_uploader.run(running_time)
