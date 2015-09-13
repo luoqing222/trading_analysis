744d0c0a0e32a19ddd07918a135bdaf2ee556917
Qing <luoqing222@gmail.com>
2015-09-13 10:42:12 -0700

    show the plot for close price and average weighted price

---
+++ data_analyser/eoddata_analyser.py
@@ -0,0 +1,64 @@
+__author__ = 'Qing'
+
+import MySQLdb
+import pandas as pd
+import pandas.io.sql as sql
+
+class EodDataAnalyser:
+    def __init__(self, host, database, user, password):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+
+    def generate_stock_closed_price_plot(self, start_date, end_date, underlying_stock, ax, color):
+        ''' function to draw the plot of stock closed price between start_date and end date, used in backward testing
+        :param start_date:
+        :param end_date:
+        :param underlying_stock:
+        :param ax:
+        :return:
+        '''
+        db = MySQLdb.connect(host=self.host, db=self.database, user=self.user, passwd=self.password)
+        table_name = "eodequity"
+
+        start_date_str = start_date.strftime('%Y-%m-%d')
+        end_date_str = end_date.strftime('%Y-%m-%d')
+
+        sql_statement = (
+            "select transaction_date, close_price from %(table_name)s where symbol"
+            "='%(underlying_stock)s' and transaction_date between '%(begin_date)s' and '%(end_date)s' "
+            "order by transaction_date asc")
+
+        data_frame = sql.read_sql(
+            sql_statement % {'begin_date': start_date_str, 'end_date': end_date_str, 'table_name': table_name, 'underlying_stock': underlying_stock}, db)
+        stock_data = pd.Series(list(data_frame['close_price']),index = data_frame['transaction_date'])
+        stock_data.plot(kind = 'line', ax = ax, title = underlying_stock,color = color)
+        db.close()
+
+    def generate_stock_weighted_average_price_plot(self,start_date, end_date, underlying_stock, ax, color):
+        ''' function to draw the plot of stock closed price between start_date and end date, used in backward testing
+        :param start_date:
+        :param end_date:
+        :param underlying_stock:
+        :param ax:
+        :return:
+        '''
+        db = MySQLdb.connect(host=self.host, db=self.database, user=self.user, passwd=self.password)
+        table_name = "dailyvolumeweightedprice"
+
+        start_date_str = start_date.strftime('%Y-%m-%d')
+        end_date_str = end_date.strftime('%Y-%m-%d')
+
+        sql_statement = (
+            "select transaction_date, average_price from %(table_name)s where symbol"
+            "='%(underlying_stock)s' and transaction_date between '%(begin_date)s' and '%(end_date)s' "
+            "order by transaction_date asc")
+
+        data_frame = sql.read_sql(
+            sql_statement % {'begin_date': start_date_str, 'end_date': end_date_str, 'table_name': table_name, 'underlying_stock': underlying_stock}, db)
+        stock_data = pd.Series(list(data_frame['average_price']),index = data_frame['transaction_date'])
+        stock_data.plot(kind = 'line', ax = ax, title = underlying_stock,color = color)
+        db.close()
+
+
---
+++ tests/eoddata_analyser_test.py
@@ -0,0 +1,28 @@
+__author__ = 'Qing'
+
+from data_analyser import eoddata_analyser
+import configparser
+import os
+import datetime
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    config_file = os.path.dirname(os.getcwd())+"/"+"option_data_management_setting.ini"
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    host = config.get("database", "host")
+    database = config.get("database", "database")
+    user = config.get("database", "user")
+    password = config.get("database", "passwd")
+
+    eod_data_analyser=eoddata_analyser.EodDataAnalyser(host, database, user, password)
+
+    start_date = datetime.datetime(year=2015, month=05, day=01)
+    end_date = datetime.datetime.now()
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    eod_data_analyser.generate_stock_closed_price_plot(start_date,end_date,"ADPT",ax, 'red')
+    eod_data_analyser.generate_stock_weighted_average_price_plot(start_date,end_date,"ADPT",ax, 'blue')
+    plt.show()
+
