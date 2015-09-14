6e9f646df8ced45613ae8b77659458d49af0de76
Qing <luoqing222@gmail.com>
2015-09-13 21:50:09 -0700

    create pdf for close price and weighted average price

--- tests/eoddata_analyser_test.py
+++ tests/eoddata_analyser_test.py
@@ -6,6 +6,7 @@ from data_analyser import eoddata_analyser
 import configparser
 import datetime
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 if __name__ == "__main__":
     config_file = os.path.dirname(os.getcwd())+"/"+"option_data_management_setting.ini"
@@ -21,9 +22,15 @@ if __name__ == "__main__":
 
     start_date = datetime.datetime(year=2015, month=05, day=01)
     end_date = datetime.datetime.now()
-    fig = plt.figure()
+    fig = plt.figure(figsize=(16,6))
     ax = fig.add_subplot(1, 1, 1)
     eod_data_analyser.generate_stock_closed_price_plot(start_date,end_date,"ADPT",ax, 'red')
     eod_data_analyser.generate_stock_weighted_average_price_plot(start_date,end_date,"ADPT",ax, 'blue')
-    plt.show()
+
+    #plt.savefig(os.path.dirname(os.getcwd())+"/messages/test.png", dpi = 100, figsize=(18,18))
+    folder = os.path.dirname(os.getcwd())+"/messages"
+    with PdfPages(folder+"/"+"test1.pdf") as pdf:
+        pdf.savefig()
+    plt.close()
+    #plt.show()
 
