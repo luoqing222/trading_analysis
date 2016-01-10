829a405b1399a4563ff7e12f667c23f21be526b6
Qing <luoqing222@gmail.com>
2016-01-10 10:47:06 -0800

    add the funciton to download the yahoo  key statistics data

--- data_collectors/yahoo_keystatistics_data_collector.py
+++ data_collectors/yahoo_keystatistics_data_collector.py
@@ -4,13 +4,17 @@ import logging
 import BeautifulSoup
 import urllib
 import datetime
-
+import re
+import os
 
 logger = logging.getLogger(__name__)
 
 class YahooKeyStatDataCollector:
     def __init__(self, stock_list):
         self.stock_list = stock_list
+        self.headers =["Shares Outstanding", "Float:", "% Held by Insiders","% Held by Institutions", "Shares Short (as of",
+                       "Short Ratio (as of", "Short % of Float (as of", "Shares Short (prior month)"]
+
 
     # function to the yahoo link for key statistics page
     @staticmethod
@@ -19,19 +23,61 @@ class YahooKeyStatDataCollector:
         return link
 
     # function to find the key statistics for the symbol
-    def web_crawler(self, symbol):
+    def web_crawler(self, symbol, output_file):
         print "loading yahoo statistics for "+symbol
         html_text = urllib.urlopen(self.get_web_page_link(symbol))
         soup = BeautifulSoup.BeautifulSoup(html_text)
         title = soup.find(text ='Share Statistics')
         table= title.parent.parent.parent.parent
+        content = {}
         for row in table.findAll("tr"):
             cells=row.findAll("td")
             if len(cells)==2:
-                for cell in cells:
-                    print cell.text.strip()
+                #print cells[0].text.strip()
+                for head in self.headers:
+                    if head in cells[0].text.strip():
+                        #print cells[0].text.strip()
+                        #print cells[1].text.strip()
+                        content[head]=self.convert_to_number(cells[1].text.strip())
+                        break
+        for head in self.headers:
+            if head in content:
+                output_file.write(content[head])
+            else:
+                output_file.write(-99)
 
-    def run(self):
-        for symbol in self.stock_list:
-            self.web_crawler(symbol)
+        output_file.flush()
+        #for key, value in content.iteritems():
+        #    print key, value
+
+
+
+    def run(self, running_time, des_folder):
+        path = des_folder+ "/daily_run/" + running_time.strftime('%Y_%m_%d')+"/yahoo/"
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        transaction_date = running_time.strftime('%Y%m%d')
+        des_file_name= path+"Key_Statistics_"+ transaction_date +".csv"
+        with open(des_file_name, "w") as output_file:
+            for symbol in self.stock_list:
+                self.web_crawler(symbol, output_file)
+                #time.sleep(2)
+
+    def convert_to_number(self, number_string):
+        pattern_match = re.match("\d*\.?\d*[Mm]", number_string)
+        if pattern_match:
+            return int(float(number_string[:-1])*1000000)
+        pattern_match=re.match("\d*\.?\d*[Bb]", number_string)
+        if pattern_match:
+            return int(float(number_string[:-1])*1000000000)
+        pattern_match=re.match("\d*\.?\d*%", number_string)
+        if pattern_match:
+            return float(number_string[:-1])*0.01
+        pattern_match=re.match("\d*\.?\d*", number_string)
+        if pattern_match:
+            return float(number_string)
+
+        return -99.0
+
 
--- tests/yahoo_keystatistics_data_collector_test.py
+++ tests/yahoo_keystatistics_data_collector_test.py
@@ -1,12 +1,14 @@
 __author__ = 'Qing'
 
-import sys,os
+import sys, os
+import re
+
 sys.path.append(os.path.realpath('..'))
 from data_collectors import yahoo_keystatistics_data_collector
 
-if __name__ == "__main__":
-    stock_list = ['MCO']
-    data_collector=yahoo_keystatistics_data_collector.YahooKeyStatDataCollector(stock_list)
-    data_collector.run()
 
+if __name__ == "__main__":
+    stock_list = ['FB']
+    data_collector = yahoo_keystatistics_data_collector.YahooKeyStatDataCollector(stock_list)
+    data_collector.run()
 
