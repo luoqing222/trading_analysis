cd943f6a2df6551b64eb55941c6bb7f27e24c313
qingluo <luoqing222@gmail.com>
2016-01-18 22:27:37 -0800

    format change in the trading_analysis.py

--- trading_analysis.py
+++ trading_analysis.py
@@ -30,11 +30,10 @@ from data_analyser import log_analyser
 import daily_back_up
 
 
-
 def send_email(file_name, mail_list, folder):
     gm = emailprocessing.Gmail('raki1978wmc6731@gmail.com', 'Fapkc1897Fapkc')
 
-    #subject = "analysis report on " + datetime.datetime.now().strftime('%m_%d_%Y')
+    # subject = "analysis report on " + datetime.datetime.now().strftime('%m_%d_%Y')
     subject = file_name
     message = "This is a test message"
     # to = "luoqing222@gmail.com"
@@ -42,6 +41,7 @@ def send_email(file_name, mail_list, folder):
     # gm.send_message(subject, message, 'luoqing222@gmail.com')
     for to in mail_list:
         gm.send_text_attachment(subject, to, file_name, folder)
+
 
 #
 # def send_email2():
@@ -127,100 +127,105 @@ if __name__ == "__main__":
         os.makedirs(message_folder)
 
     running_time = datetime.datetime.now()
-    #running_time = datetime.datetime(2015,12,9,0,0,0)
+    # running_time = datetime.datetime(2015,12,9,0,0,0)
     log_file_name = "daily_run.log"
-    log_file = message_folder+"/"+log_file_name
-    logging.basicConfig(filename=log_file, level=logging.INFO,filemode="w")
-    logger.info("start data collection process on "+running_time.strftime('%m_%d_%Y'))
+    log_file = message_folder + "/" + log_file_name
+    logging.basicConfig(filename=log_file, level=logging.INFO, filemode="w")
+    logger.info("start data collection process on " + running_time.strftime('%m_%d_%Y'))
     mail_list = ["luoqing222@gmail.com"]
 
     config_file = "option_data_management_setting.ini"
     config = configparser.ConfigParser()
     config.read(config_file)
 
-    #to download eod data
+    # to download eod data
     try:
         logger.info("Start collecting eod data")
         eod_data_manager = eoddata_data_manager.EodDataDataManager()
         eod_data_manager.daily_run()
         logger.info("eod data is successfully downloaded")
     except Exception, e:
-        logger.warning("exception is thrown when downloading eod data:"+str(e))
+        logger.warning("exception is thrown when downloading eod data:" + str(e))
 
-    #to download yahoo option data
+    # to download yahoo option data
     try:
         logger.info("Start collecting yahoo option data")
         option_data_manager = yahoo_option_data_manager.YahooOptionDataManager()
         option_data_manager.daily_run()
         logger.info("yahoo option data is successfully downloaded")
     except Exception, e:
-        logger.warning("exception is thrown when downloading yahoo option data:"+str(e))
+        logger.warning("exception is thrown when downloading yahoo option data:" + str(e))
 
-    #to download yahoo key statistics data
+    # to download yahoo key statistics data
     try:
         logger.info("Start collecting yahoo key statistics data")
-        des_folder = config.get("csv","data_folder")
+        des_folder = config.get("csv", "data_folder")
         host = config.get("database", "host")
         database = config.get("database", "database")
         user = config.get("database", "user")
         password = config.get("database", "passwd")
-        nasdaq_list = trading_data_utility_by_sql.TradingDataUtilityBySQL(host,database, user, password).get_nasdaq_list(running_time)
-        nyse_list= trading_data_utility_by_sql.TradingDataUtilityBySQL(host,database, user, password).get_nyse_list(running_time)
-        stock_list = nasdaq_list+nyse_list
+        nasdaq_list = trading_data_utility_by_sql.TradingDataUtilityBySQL(host, database, user,
+                                                                          password).get_nasdaq_list(running_time)
+        nyse_list = trading_data_utility_by_sql.TradingDataUtilityBySQL(host, database, user, password).get_nyse_list(
+            running_time)
+        stock_list = nasdaq_list + nyse_list
         data_collector = yahoo_keystatistics_data_collector.YahooKeyStatDataCollector(stock_list)
         data_collector.run(running_time, des_folder)
         logger.info("yahoo key statistics data is successfully downloaded")
     except Exception, e:
-        logger.warnning("exception is thrown when downloading yahoo key statistics data:"+str(e))
+        logger.warnning("exception is thrown when downloading yahoo key statistics data:" + str(e))
 
-    #to download 1 bar min data
+    # to download 1 bar min data
     try:
         logger.info("Start collecting 1 minute bar data from eod")
         driver_location = config.get("driver", "chrome_driver")
-        download_folder = config.get("driver","download_folder")
+        download_folder = config.get("driver", "download_folder")
         username = config.get("eod", "user")
         password = config.get("eod", "passwd")
-        des_folder = config.get("csv","data_folder")
-        data_collector = eod_1minbar_data_collector.Eod1MinBarDataCollector(driver_location,username,password)
+        des_folder = config.get("csv", "data_folder")
+        data_collector = eod_1minbar_data_collector.Eod1MinBarDataCollector(driver_location, username, password)
         data_collector.run(download_folder, des_folder, running_time)
         logger.info("eod 1 min bar data is successfully downloaded")
     except Exception, e:
-        logger.warning("exception is thrown when downloading eod 1 minute bar data: "+str(e))
+        logger.warning("exception is thrown when downloading eod 1 minute bar data: " + str(e))
 
-    #to upload 1 bar min data into database
+    # to upload 1 bar min data into database
     try:
-         host = config.get("database", "host")
-         database = config.get("database", "database")
-         user = config.get("database", "user")
-         password = config.get("database", "passwd")
-         des_folder = config.get("csv","data_folder")
-         data_uploader = eod_1minbar_data_uploader.Eod1MinBarDataUploader(host, database, user,password, des_folder)
-         data_uploader.run(running_time)
-    except Exception, e:
-        logger.warning("exception is thrown when uploading eod 1 minute bar data: "+str(e))
-
-    #to get the news count data
-    try:
-        des_folder = config.get("csv","data_folder")
         host = config.get("database", "host")
         database = config.get("database", "database")
         user = config.get("database", "user")
         password = config.get("database", "passwd")
-        des_folder = config.get("csv","data_folder")
-        nasdaq_list = trading_data_utility_by_sql.TradingDataUtilityBySQL(host,database, user, password).get_nasdaq_list(running_time)
-        nyse_list= trading_data_utility_by_sql.TradingDataUtilityBySQL(host,database, user, password).get_nyse_list(running_time)
+        des_folder = config.get("csv", "data_folder")
+        data_uploader = eod_1minbar_data_uploader.Eod1MinBarDataUploader(host, database, user, password, des_folder)
+        data_uploader.run(running_time)
+    except Exception, e:
+        logger.warning("exception is thrown when uploading eod 1 minute bar data: " + str(e))
+
+    # to get the news count data
+    try:
+        des_folder = config.get("csv", "data_folder")
+        host = config.get("database", "host")
+        database = config.get("database", "database")
+        user = config.get("database", "user")
+        password = config.get("database", "passwd")
+        des_folder = config.get("csv", "data_folder")
+        nasdaq_list = trading_data_utility_by_sql.TradingDataUtilityBySQL(host, database, user,
+                                                                          password).get_nasdaq_list(running_time)
+        nyse_list = trading_data_utility_by_sql.TradingDataUtilityBySQL(host, database, user, password).get_nyse_list(
+            running_time)
         data_collector = google_news_data_collector.GoogleNewsDataCollector(driver_location=des_folder,
-                                                                        nyse_list=nyse_list, nasdaq_list=nasdaq_list)
-        data_collector.run(running_time,des_folder)
+                                                                            nyse_list=nyse_list,
+                                                                            nasdaq_list=nasdaq_list)
+        data_collector.run(running_time, des_folder)
         logger.info("Google News Count Downloaded")
     except Exception, e:
-        logger.warning("Google News Count Failed: "+str(e))
+        logger.warning("Google News Count Failed: " + str(e))
 
     send_email(log_file_name, mail_list, message_folder)
 
     # to backup the data on Amazon
     keyword_list = ['WARNING']
-    log_analyser=log_analyser.LogAnalyser(message_folder, log_file_name,keyword_list)
+    log_analyser = log_analyser.LogAnalyser(message_folder, log_file_name, keyword_list)
     # if everything is OK, then backup the data at Amazon
     if not log_analyser.findKeyWord():
         src_folder = config.get("csv", "data_folder") + "/" + "daily_run" + "/" + running_time.strftime("%Y_%m_%d")
@@ -230,11 +235,9 @@ if __name__ == "__main__":
         zip_file_name = running_time.strftime("%Y_%m_%d") + ".zip"
         daily_back_up.zip_daily_data(src_folder, des_folder, zip_file_name)
         daily_back_up.GlacierVault(daily_back_up.VAULT_NAME).upload(des_folder + "/" + zip_file_name)
-        #GlacierVault(VAULT_NAME).retrieve(des_folder + "/" + zip_file_name)
-        #GlacierVault(VAULT_NAME).delete(des_folder + "/" + zip_file_name)
-        #print GlacierVault(VAULT_NAME).get_archives_name()
-        #print GlacierVault(vault_name).get_archive_id(des_folder + "/" + zip_file_name)
+        # GlacierVault(VAULT_NAME).retrieve(des_folder + "/" + zip_file_name)
+        # GlacierVault(VAULT_NAME).delete(des_folder + "/" + zip_file_name)
+        # print GlacierVault(VAULT_NAME).get_archives_name()
+        # print GlacierVault(vault_name).get_archive_id(des_folder + "/" + zip_file_name)
         mail_list = ["luoqing222@gmail.com"]
         send_email(daily_back_up.SHELVE_FILE_NAME, mail_list, os.path.expanduser("~"))
-
-
