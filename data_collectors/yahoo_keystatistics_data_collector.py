6cdd07f10d40ed706929d4bfda3533be3bd779a2
qingluo <luoqing222@gmail.com>
2016-01-18 22:18:31 -0800

    add collectors of yahoo key statistics data

--- data_collectors/yahoo_keystatistics_data_collector.py
+++ data_collectors/yahoo_keystatistics_data_collector.py
@@ -23,34 +23,34 @@ class YahooKeyStatDataCollector:
         return link
 
     # function to find the key statistics for the symbol
-    def web_crawler(self, symbol, output_file):
+    def web_crawler(self, symbol, output_file, running_time):
         print "loading yahoo statistics for "+symbol
-        html_text = urllib.urlopen(self.get_web_page_link(symbol))
-        soup = BeautifulSoup.BeautifulSoup(html_text)
-        title = soup.find(text ='Share Statistics')
-        table= title.parent.parent.parent.parent
-        content = {}
-        for row in table.findAll("tr"):
-            cells=row.findAll("td")
-            if len(cells)==2:
-                #print cells[0].text.strip()
-                for head in self.headers:
-                    if head in cells[0].text.strip():
-                        #print cells[0].text.strip()
-                        #print cells[1].text.strip()
-                        content[head]=self.convert_to_number(cells[1].text.strip())
-                        break
-        for head in self.headers:
-            if head in content:
-                output_file.write(content[head])
-            else:
-                output_file.write(-99)
-
-        output_file.flush()
+        try:
+            html_text = urllib.urlopen(self.get_web_page_link(symbol))
+            soup = BeautifulSoup.BeautifulSoup(html_text)
+            title = soup.find(text ='Share Statistics')
+            table= title.parent.parent.parent.parent
+            content = {}
+            for row in table.findAll("tr"):
+                cells=row.findAll("td")
+                if len(cells)==2:
+                    for head in self.headers:
+                        if head in cells[0].text.strip():
+                            content[head]=self.convert_to_number(cells[1].text.strip())
+                            break
+            for head in self.headers:
+                if head in content:
+                    output_file.write(str(content[head]))
+                    output_file.write(",")
+                else:
+                    output_file.write("-99,")
+            output_file.write(symbol+","+running_time.strftime("%Y-%m-%d")+",")
+            output_file.write("\n")
+            output_file.flush()
+        except:
+            pass
         #for key, value in content.iteritems():
         #    print key, value
-
-
 
     def run(self, running_time, des_folder):
         path = des_folder+ "/daily_run/" + running_time.strftime('%Y_%m_%d')+"/yahoo/"
@@ -60,17 +60,26 @@ class YahooKeyStatDataCollector:
         transaction_date = running_time.strftime('%Y%m%d')
         des_file_name= path+"Key_Statistics_"+ transaction_date +".csv"
         with open(des_file_name, "w") as output_file:
+            for head in self.headers:
+                output_file.write(head+",")
+            output_file.write("symbol,date,")
+            output_file.write("\n")
             for symbol in self.stock_list:
-                self.web_crawler(symbol, output_file)
+                self.web_crawler(symbol, output_file, running_time)
                 #time.sleep(2)
 
     def convert_to_number(self, number_string):
+        if number_string=="N/A":
+            return -99
         pattern_match = re.match("\d*\.?\d*[Mm]", number_string)
         if pattern_match:
             return int(float(number_string[:-1])*1000000)
         pattern_match=re.match("\d*\.?\d*[Bb]", number_string)
         if pattern_match:
             return int(float(number_string[:-1])*1000000000)
+        pattern_match=re.match("\d*\.?\d*[Kk]", number_string)
+        if pattern_match:
+            return int(float(number_string[:-1])*1000)
         pattern_match=re.match("\d*\.?\d*%", number_string)
         if pattern_match:
             return float(number_string[:-1])*0.01
@@ -78,6 +87,6 @@ class YahooKeyStatDataCollector:
         if pattern_match:
             return float(number_string)
 
-        return -99.0
+        return -99
 
 
--- tests/yahoo_keystatistics_data_collector_test.py
+++ tests/yahoo_keystatistics_data_collector_test.py
@@ -2,13 +2,31 @@ __author__ = 'Qing'
 
 import sys, os
 import re
+import configparser
+import datetime
 
 sys.path.append(os.path.realpath('..'))
 from data_collectors import yahoo_keystatistics_data_collector
-
+import trading_data_utility_by_sql
 
 if __name__ == "__main__":
-    stock_list = ['FB']
+    config_file = os.path.dirname(os.getcwd())+"/"+"option_data_management_setting.ini"
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    des_folder = config.get("csv","data_folder")
+    host = config.get("database", "host")
+    database = config.get("database", "database")
+    user = config.get("database", "user")
+    password = config.get("database", "passwd")
+
+    des_folder = config.get("csv","data_folder")
+
+    running_time= datetime.datetime.now()
+    nasdaq_list = trading_data_utility_by_sql.TradingDataUtilityBySQL(host,database, user, password).get_nasdaq_list(running_time)
+    nyse_list= trading_data_utility_by_sql.TradingDataUtilityBySQL(host,database, user, password).get_nyse_list(running_time)
+
+    stock_list = nasdaq_list+nyse_list
     data_collector = yahoo_keystatistics_data_collector.YahooKeyStatDataCollector(stock_list)
-    data_collector.run()
+    data_collector.run(running_time, des_folder)
 
