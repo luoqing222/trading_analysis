09d38bb4d7cebd47518633fbc6c5646575a7deb1
qingluo <luoqing222@gmail.com>
2016-07-17 17:39:53 -0700

    test the chrome driver in linux

--- tests/yahoo_option_data_collector_new_test.py
+++ tests/yahoo_option_data_collector_new_test.py
@@ -7,22 +7,26 @@ sys.path.append(os.path.realpath('..'))
 from data_collectors import yahoo_option_data_collector_new
 
 if __name__ == "__main__":
+
+    symbol="FB"
+
     if platform.system() == "Windows":
         print "Windows"
     elif platform.system() == "Linux":
         print "Linux"
-    else:
-        print "others"
+        driver = webdriver.Chrome()
+        data_collector = yahoo_option_data_collector_new.YahooOptionDataCollector(driver)
+        data_collector.download_options(symbol)
+        driver.quit()
 
-    #symbol="FB"
+    else:
+        print "Operating System Untested"
+
+
 
     #code under windows
 
     # code under linux
-    # driver = webdriver.Chrome()
-    #data_collector = yahoo_option_data_collector_new.YahooOptionDataCollector(driver)
-    #data_collector.download_options(symbol)
-    #driver.quit()
 
 
 
